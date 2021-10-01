7c5293e02f44e6a06f0cecdda8e38179227e1056
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-10-01 15:01:33 -0300

    test(signup): garante que SignUpController retorne 400 caso o  nome nao seja fornecido

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from "./Signup";
+
+describe('Signup Controller', () => {
+    test('should return 400 if no name is provided', ()=>{
+        const sut = new SignUpController();
+        const httpRequest = {
+            body:{
+                
+                email: 'any_email@mail.com',
+                password: 'any_123',
+                ásswordConfirmation: 'any_123'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
+    
+});
+
