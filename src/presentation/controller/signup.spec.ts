a92dfa4639feec8247c6f24f1c81d54e9cc02c70
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-10-01 17:16:46 -0300

    refactor(signup): ensure SignUpController.handle implement correct intefaces

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -15,6 +15,20 @@ describe('Signup Controller', () => {
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new Error('Missing param: name'));
     });
+    test('should return 400 if no name is provided', ()=>{
+        const sut = new SignUpController();
+        const httpRequest = {
+            body:{
+                
+                name: 'any_name',
+                password: 'any_123',
+                ásswordConfirmation: 'any_123'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    });
     
 });
 
---
+++ src/presentation/protocols/http.ts
@@ -0,0 +1,12 @@
+interface HttpResponse{
+    statusCode:number
+    body: any
+}
+
+export {HttpResponse}
+
+interface HttpRequest{
+    body?: any
+}
+
+export {HttpRequest}
