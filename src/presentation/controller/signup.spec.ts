c1be5cd7465303874fd82b90e0e45824ae4efc75
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-10-04 14:56:36 -0300

    test(signup): ensure SignUpController retunrs 400 if no password is provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -9,7 +9,7 @@ describe('Signup Controller', () => {
                 
                 email: 'any_email@mail.com',
                 password: 'any_123',
-                ásswordConfirmation: 'any_123'
+                passwordConfirmation: 'any_123'
             }
         }
         const httpResponse = sut.handle(httpRequest);
@@ -23,12 +23,26 @@ describe('Signup Controller', () => {
                 
                 name: 'any_name',
                 password: 'any_123',
-                ásswordConfirmation: 'any_123'
+                passwordConfirmation: 'any_123'
             }
         }
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError('email'));
+    });
+    
+    test('should return 400 if no password is provided', ()=>{
+        const sut = new SignUpController();
+        const httpRequest = {
+            body:{                
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                passwordConfirmation: 'any_123'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('password'));
     });
     
 });
