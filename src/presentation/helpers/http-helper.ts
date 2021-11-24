422da69d3f2006b5b8428c00a7a6ea0c366cc0bf
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-24 16:44:01 -0300

    refactor(servererror): change import error

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
 
-import { ServerError } from "../error/ServerError";
+import { ServerError } from "../error/";
 import { HttpResponse } from "../protocols/http";
 export const badRequest = (error: Error): HttpResponse => ({
     statusCode: 400,
